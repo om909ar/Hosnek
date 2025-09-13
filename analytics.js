76bb29b0e37db96a9e6f794b38c814aeff04c367
om909ar <139689383+om909ar@users.noreply.github.com>
2025-09-13 19:08:13 +0300

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,12 +1,9 @@
-(function(){
-  var s = document.createElement('script');
-  s.async = true;
-  s.src = "https://www.googletagmanager.com/gtag/js?id=G-DZKQS9XHEH";
-  document.head.appendChild(s);
-
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DZKQS9XHEH"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
-  window.gtag = gtag;
   gtag('js', new Date());
+
   gtag('config', 'G-DZKQS9XHEH');
-})();
+</script>
