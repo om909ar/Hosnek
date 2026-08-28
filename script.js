e2a409a41e8b4a55a01423afbed4f6f9dd4ac45d
om909ar <139689383+om909ar@users.noreply.github.com>
2026-08-28 13:10:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ function showAzkar(timeOfDay) {
   }
 
   container.innerHTML = `
-    <div class="azkar-card" style="font-size:18px !important; line-height:2 !important;">
+    <div class="azkar-card">
       ${azkarText.replace(/\n/g, '<br/>')}
     </div>
   `;
